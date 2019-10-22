865b33b832622c332e973677867a5b0f6ee53f54
rashmi <rashmi.manandhar7@gmail.com>
2019-10-22 13:42:35 -0400

    using getters inside getters

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
@@ -9,13 +9,33 @@ export default new Vuex.Store({
       id: 'abc',
       name: 'Rashmi'
     },
-    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community']
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
   },
   mutations: {},
   actions: {},
   getters: {
     catLength: state => {
       return state.categories.length
+    },
+    doneToDos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneToDos.length
     }
   }
-});
+})
