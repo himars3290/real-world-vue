68b827a4da3936760e82d0e2c961098d591a274b
rashmi <rashmi.manandhar7@gmail.com>
2019-10-22 15:36:24 -0400

    create an event and store in vuex state

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from "./services/EventService";
 
 Vue.use(Vuex)
 
@@ -31,8 +32,17 @@ export default new Vuex.Store({
       { id: 4, title: '...', organizer: '...' }
     ]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event){
+      state.events.push(event);
+    }
+  },
+  actions: {
+    createEvent({commit}, event){
+      EventService.postEvent(event);
+      commit('ADD_EVENT', event);
+    }
+  },
   getters: {
     catLength: state => {
       return state.categories.length
