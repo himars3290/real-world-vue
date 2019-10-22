2776d82896e47e7c39c28f5696dfb92756066230
rashmi <rashmi.manandhar7@gmail.com>
2019-10-22 13:39:28 -0400

    using getters from vuex store

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>Event Create</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">
+        {{ category }}
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState({
+      user: 'user',
+      categories: 'categories'
+    })
+  }
+}
+</script>
+
+<style scoped></style>
