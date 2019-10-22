e28fb920ee7f5fc96c8d4a3effff178eae267e37
rashmi <rashmi.manandhar7@gmail.com>
2019-10-22 13:06:58 -0400

    get events from api using axios and show the list

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
     <h1>Event List</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event">
+      {{event}}
+    </EventCard>
     <BaseIcon></BaseIcon>
   </div>
 </template>
 
 <script>
-import EventCard from '@/components/EventCard.vue'
+  import EventCard from '@/components/EventCard.vue'
+  import axios from 'axios'
 
-export default {
-  components: {
-    EventCard
+  export default {
+    components: {
+      EventCard
+    },
+    data() {
+      return {
+        events: []
+      }
+    },
+    created() {
+      axios.get('http://localhost:8080/db.json')
+        .then(response => {
+          this.events = response.data.events;
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
   }
-}
 </script>
