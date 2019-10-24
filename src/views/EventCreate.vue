c949f0b66cab3179d7c1b4a0ba495fb494517b9f
rashmi <rashmi.manandhar7@gmail.com>
2019-10-24 17:18:14 -0400

    validate form using vuelidate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -6,7 +6,15 @@
         label="Select a category"
         :options="categories"
         v-model="event.category"
+        :class="{ error: $v.event.category.$error }"
+        @blur="$v.event.category.$touch()"
       />
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">
+          Category is required
+        </p>
+      </template>
+
       <h3>Name & describe your event</h3>
       <BaseInput
         label="Title"
@@ -14,14 +22,28 @@
         type="text"
         placeholder="Title"
         class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
       />
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">
+          Title is required
+        </p>
+      </template>
       <BaseInput
         label="Description"
         v-model="event.description"
         type="text"
         placeholder="Add a description"
         class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">
+          Description is required
+        </p>
+      </template>
 
       <h3>Where is your event?</h3>
       <BaseInput
@@ -30,16 +52,35 @@
         type="text"
         placeholder="Add a location"
         class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"
       />
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">
+          Location is required
+        </p>
+      </template>
 
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
-      <BaseSelect label="Select a time" :options="times" v-model="event.time" class="field"/>
+      <BaseSelect
+        label="Select a time"
+        :options="times"
+        v-model="event.time"
+        class="field"
+        :class="{ error: $v.event.time.$error }"
+        @blur="$v.event.time.$touch()"
+      />
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">
+          TIme is required
+        </p>
+      </template>
 
-<!--      <input type="submit" class="button -fill-gradient" value="Submit" />-->
+      <!--      <input type="submit" class="button -fill-gradient" value="Submit" />-->
       <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
     </form>
   </div>
@@ -50,6 +91,7 @@ import Datepicker from 'vuejs-datepicker'
 import Nprogress from 'nprogress'
 import BaseInput from '../components/BaseInput'
 import BaseSelect from '../components/BaseSelect'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   data() {
@@ -100,6 +142,16 @@ export default {
         time: '',
         attendees: []
       }
+    }
+  },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required }
     }
   }
 }
