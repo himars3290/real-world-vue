b9be856476e2e8c1e268919104c42ff6b71eebfd
rashmi <rashmi.manandhar7@gmail.com>
2019-10-24 17:47:33 -0400

    create a mixin to refactor the common code in BaseInput and BaseSelect components

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,36 +1,30 @@
 <template>
   <div>
-    <label v-if="label">{{label}}</label>
-    <input :value="value" @input="updateValue" v-bind="$attrs" v-on="listeners">
+    <label v-if="label">{{ label }}</label>
+    <input
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    inheritAttrs: false,
-    props: {
-      label: {
-        type: String,
-        default: ""
-      },
-      value: [String, Number]
-    },
-    computed: {
-      listeners() {
-        return {
-          ...this.$listeners,
-          input: this.updateValue
-        }
-      }
-    },
-    methods: {
-      updateValue(event){
-        this.$emit('input', event.target.value);
+import { formFieldMixin } from '../mixins/formFieldMixin'
+
+export default {
+  mixins: [formFieldMixin],
+
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
       }
     }
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,32 +1,36 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <select :value="value" @change="updateValue" v-bind="$attrs" v-on="$listeners">
-      <option v-for="option in options" :key="option" :selected="option === value">{{ option }}</option>
+    <select
+        :value="value"
+        @change="updateValue"
+        v-bind="$attrs"
+        v-on="$listeners"
+    >
+      <option
+          v-for="option in options"
+          :key="option"
+          :selected="option === value"
+      >{{ option }}
+      </option
+      >
     </select>
   </div>
 </template>
 
 <script>
-  export default {
-    inheritAttrs: false,
-    props: {
-      options: {
-        type: Array,
-        required: true
-      },
-      label: {
-        type: String,
-        default: ''
-      },
-      value: [String, Number]
+  import {formFieldMixin} from "../mixins/formFieldMixin";
+
+export default {
+  mixins: [formFieldMixin],
+
+  props: {
+    options: {
+      type: Array,
+      required: true
     },
-    methods: {
-      updateValue(event) {
-        this.$emit('input', event.target.value)
-      }
-    }
   }
+}
 </script>
 
 <style scoped></style>
